32c1c5855569e792181979d0e81ac056f7e02e9b
yotamm <yotamm@il.ibm.com>
2016-04-26 11:03:59 +0300

    revert to the spring option

--- src/test/java/net/mfpdev/MyAdapterIT.java
+++ src/test/java/net/mfpdev/MyAdapterIT.java
@@ -1,41 +1,35 @@
-/**
- *    © Copyright 2016 IBM Corp.
- *
- *    Licensed under the Apache License, Version 2.0 (the "License");
- *    you may not use this file except in compliance with the License.
- *    You may obtain a copy of the License at
- *
- *        http://www.apache.org/licenses/LICENSE-2.0
- *
- *    Unless required by applicable law or agreed to in writing, software
- *    distributed under the License is distributed on an "AS IS" BASIS,
- *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *    See the License for the specific language governing permissions and
- *    limitations under the License.
- */
-
 package net.mfpdev;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.RestTemplate;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = MyAdapterIT.class)
+@Configuration
+@PropertySource("classpath:/defaults.properties")
 public class MyAdapterIT {
 
-    String adapterURL = getServerURL()+"/api/adapters/adapterWithAutoTests";
-
-    private static String getServerURL() {
-        String value = System.getProperty("mfp-server-url");
-        if (value == null){
-            return "http://localhost:9080/mfp";
-        }
-        return value;
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyConfigInDev() {
+        return new PropertySourcesPlaceholderConfigurer();
     }
+
+    @Value("${mfp-runtime-url}/api/adapters/adapterWithAutoTests")
+    String adapterURL;
 
     @Test
     public void testUnprotectedResource(){
         RestTemplate restTemplate = new RestTemplate();
         String response = restTemplate.getForObject(adapterURL+"/resource/unprotected/ttt", String.class);
-        Assert.assertEquals("Hello: ttt", response);
+        Assert.assertEquals("Hello2: ttt", response);
     }
 }
